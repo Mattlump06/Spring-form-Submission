3f2d05f72b56af34950e78281c8536728d2edf66
Mattlump06 <mattlumpkin06@gmail.com>
2021-01-12 02:49:50 -0500

    Implemented Greeting Domain Implemented Greeting Controller Implemented Greeting and result views Needs work on styling

---
+++ src/main/java/com/nerdsthink/springform/domain/Greeting.java
@@ -0,0 +1,14 @@
+package com.nerdsthink.springform.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Greeting {
+
+    private long id;
+    private String content;
+
+
+}
