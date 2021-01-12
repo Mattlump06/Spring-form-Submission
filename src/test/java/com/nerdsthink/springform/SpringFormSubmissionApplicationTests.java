1341349d2a865924660a9e8a98ec69880ae17644
Mattlump06 <mattlumpkin06@gmail.com>
2021-01-12 00:16:21 -0500

    initial commit...

---
+++ src/test/java/com/nerdsthink/springform/SpringFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdsthink.springform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFormSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
